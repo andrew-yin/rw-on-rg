79311128f699ed329692ca5922333905f325c40d
anecnhoj53 <98916161+anecnhoj53@users.noreply.github.com>
2022-02-07 17:35:14 -0600

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -29,6 +29,40 @@ class Simulator:
         plt.xlabel("# of Steps")
         plt.ylabel("Proportion of visited vertices")
         plt.show()
+        
+        
+@staticmethod
+    def plot_rw_coverage_multisamples(n,p,steps,k):
+        '''
+        plot the average of each steps
+        '''
+        proportion_sum = np.array(np.zeros(steps+1))
+        for j in range(k):
+            graph = Graph.get_er_random_graph(n, p)           
+            start = random.choice(list(graph.nodes))           
+            random_walker = Walker(strategy="random", graph=graph, start=start)
+            
+            for i in range(steps):
+                random_walker.move_dict()
+
+            proportion_cur = [1]
+            for i in range(steps):
+                if random_walker.path[i+1] in random_walker.path[0:i+1]:
+                    proportion_cur.append(proportion_cur[i])
+                else:
+                    proportion_cur.append(proportion_cur[i]+1)
+
+            # proportion_cur = [len(set(random_walker.path[:i]))/n for i in range(1,len(random_walker.path)+1)]
+
+            proportion_sum = proportion_sum + np.array(proportion_cur)/n
+
+        proportion_avg = proportion_sum / n
+        print(proportion_avg)
+        plt.plot(np.arange(0, len(proportion_avg), step=1), proportion_avg)
+        plt.title("Proportion of visited vertices vs. # of Steps")
+        plt.xlabel("# of Steps")
+        plt.ylabel("Proportion of visited vertices")
+        plt.show()  
+        
 
         
-        print('hi')
