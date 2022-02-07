f8607733ba587c9f79de1fd81bceb0cab87db9fc
anecnhoj53 <98916161+anecnhoj53@users.noreply.github.com>
2022-02-07 17:36:30 -0600

    Update walker.py

--- walker.py
+++ walker.py
@@ -7,12 +7,20 @@ class Walker:
         self.graph = graph
         self.start = start
         self.cur = start
+        self.path = [start]
+        self.neighbors = {node: list(neighbors.keys()) for node, neighbors in self.graph.adj.items()}
 
     def move(self):
         if self.strategy == "random":
             neighbors = [i[0] for i in self.graph.adj[self.cur].items()]
             if len(neighbors) != 0:
                 self.cur = random.choice(neighbors)
+
+    
+    def move_dict(self):
+        if self.strategy == "random":
+            if len(self.neighbors[self.cur]) != 0:
+                self.path.append(random.choice(self.neighbors[self.cur]))
 
     def get_current_node(self):
         return self.cur
