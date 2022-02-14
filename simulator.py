5b1e19a22c0dc760ecb0671f72f43994fd8133a7
Andrew Yin <andrewyin1@gmail.com>
2022-02-14 11:30:12 -0600

    Add check for component exclusion (#3)

--- simulator.py
+++ simulator.py
@@ -1,10 +1,8 @@
-import walkers
-from graph import Graph
-# from walkers.walker import Walker
-from walkers.random_walker import RandomWalker
-import random
 import numpy as np
 import matplotlib.pyplot as plt
+
+from graph import Graph
+from walkers import RandomWalker
 
 
 class Simulator:
@@ -19,7 +17,7 @@ class Simulator:
         n = len(graph)
 
         walk = [walker.cur]
-        proportions = np.ones(steps+1) 
+        proportions = np.ones(steps+1)
 
         # Simulate walk
         visited = set(walk)
@@ -39,67 +37,39 @@ class Simulator:
         if calculate_prop:
             results['proportions'] = proportions/n
         return results
-        
-    @staticmethod
-    def plot_rw_on_rg_visited_prop(n, p, steps, k=1):
-        """
-        Plot the average proportion of visited vertices at each step 
-        of a random walk on a random graph over k samples
-        """
-        random_walker = RandomWalker()
-        proportion_sum = np.zeros(steps+1)
-        for i in range(k):
-            graph = Graph.get_er_random_graph(n, p)           
-            results = Simulator.simulate_walk(graph, random_walker, steps)
-
-            proportion_sum += results['proportions']
-        proportion_avg = proportion_sum / k
-
-        # plt.plot(np.arange(1, len(proportion_avg)+1, step=1), proportion_avg, label='k = {:d}'.format(k))
-        # plt.title("Proportion of Visited Vertices vs. # of Steps")
-        # plt.xlabel("# of Steps")
-        # plt.ylabel("Proportion of visited vertices")
-        # plt.legend()
-        # plt.show()
-        return proportion_avg
 
     @staticmethod
-    def plot_rw_on_k_regular_visited_prop(d, n, steps, k=1):
+    def plot_rw_on_rg_visited_prop(n, p, k=1, comp_adj=True, d=None):
         """
-        Plot the average proportion of visited vertices at each step
-        of a random walk on a k-regular random graph over k samples
+        Plot the average proportion of visited vertices over n*log(n)^2 steps
+        of a random walk on a random graph over k samples.
+
+        If comp_adj is true, we exclude simulated runs that do not visit a
+        certain proportion of vertices after n*log(n)^2 steps, as you can
+        be fairly certain these simulations do not start in the largest component
+        of the graph.
         """
         random_walker = RandomWalker()
+        steps = int(n*np.log(n)*np.log(n))
         proportion_sum = np.zeros(steps+1)
-        for i in range(k):
-            graph = Graph.get_k_regular_random_graph(d, n)
-            results = Simulator.simulate_walk(graph, random_walker, steps)
 
-            proportion_sum += results['proportions']
+        f_d = largest_comp(d)
+        valid_samples = 0
+        while valid_samples < k:
+            graph = Graph.get_er_random_graph(n, p)
+            results = Simulator.simulate_walk(graph, random_walker, steps)
+            if results['proportions'][-1] >= f_d:
+                proportion_sum += results['proportions']
+                valid_samples += 1
         proportion_avg = proportion_sum / k
 
-        # plt.plot(np.arange(1, len(proportion_avg)+1, step=1), proportion_avg, label='k = {:d}'.format(k))
-        # plt.text(25, 0.9, 'n={:n}'.format(n), horizontalalignment='center')
-        # plt.title("k-regular: Proportion of Visited Vertices vs. # of Steps")
-        # plt.xlabel("# of Steps")
-        # plt.ylabel("Proportion of visited vertices")
-        # plt.legend()
-        # plt.show()
-        return proportion_avg
-
-
-if __name__ == '__main__':
-    n, steps, k = 100, 1000, 1000
-    d = 10
-    p = 10/n
-    plt.plot()
-    a = Simulator.plot_rw_on_rg_visited_prop(n, p, steps, k)
-    t = np.arange(1, len(a)+1, step=1)
-    b = Simulator.plot_rw_on_k_regular_visited_prop(d, n, steps, k)
-    plt.plot(t, a, label='Erdos-Renyi')
-    plt.plot(t, b, label='k-Regular')
-    plt.title("ER vs. k-Regular: Proportion of Visited Vertices vs. # of Steps")
-    plt.xlabel("# of Steps")
-    plt.ylabel("Proportion of visited vertices")
-    plt.legend(title='n={:n}, '.format(n)+'k={:d}'.format(k))
-    plt.show()
+        c = np.arange(0, 1, 0.1)*steps
+        avg_at_each_c = [proportion_avg[int(i)] for i in c]
+        plt.scatter(c, avg_at_each_c, c='r', s=50)
+        plt.plot(np.arange(1, len(proportion_avg)+1, step=1),
+                 proportion_avg, label='k = {:d}, n={:d}, p={:.2f}'.format(k, n, p))
+        plt.title("Proportion of Visited Vertices vs. # of Steps")
+        plt.xlabel("# of Steps")
+        plt.ylabel("Proportion of visited vertices")
+        plt.legend()
+        plt.show()
