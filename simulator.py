4f315d603e4a345c8ee91095d52f5975d0fbff55
Andrew Yin <andrewyin1@gmail.com>
2022-02-16 19:31:51 -0600

    Refactor

--- simulator.py
+++ simulator.py
@@ -1,12 +1,9 @@
-from audioop import avg
 import numpy as np
 import matplotlib.pyplot as plt
 
 from graph import Graph
-from walkers import RandomWalker
-from walkers.greedy_walker import GreedyWalker
+from walkers import RandomWalker, GreedyUnbiasedWalker
 from utils import largest_comp
-
 
 
 class Simulator:
@@ -43,7 +40,7 @@ class Simulator:
         return results
 
     @staticmethod
-    def simulate_rw_on_rg_visited_prop(n, d, k=1, comp_adj=True):
+    def simulate_rw_on_rg_visited_prop(n, p, k=1, comp_adj=True, d=None):
         """
         Simulate the average proportion of visited vertices over n*log(n)^2 steps
         of a random walk on a random graph over k samples. 
@@ -57,7 +54,7 @@ class Simulator:
         steps = int(n*np.log(n)*np.log(n))
         proportion_sum = np.zeros(steps+1)
 
-        threshold = largest_comp(d) * 0.8 
+        threshold = largest_comp(d) 
         valid_samples = 0
         while valid_samples < k:
             graph = Graph.get_er_random_graph(n, d/n)
@@ -69,8 +66,27 @@ class Simulator:
         return proportion_avg
 
     @staticmethod
+    def simulate_guw_on_rg_visited_prop(n, p, k=1, comp_adj=True, d=None):
+        greedy_walker = GreedyUnbiasedWalker()
+        steps = int(n*np.log(n)*np.log(n))
+        proportion_sum = np.zeros(steps+1)
+
+        threshold = largest_comp(d)
+        valid_samples = 0
+        while valid_samples < k:
+            graph = Graph.get_er_random_graph(n, p)
+            results = Simulator.simulate_walk(
+                graph, greedy_walker, steps)
+            if results['proportions'][-1] >= threshold:
+                proportion_sum += results['proportions']
+                valid_samples += 1
+        proportion_avg = proportion_sum / k
+        return proportion_avg
+
+    @staticmethod
     def plot_rw_on_rg_visited_prop(n, p, k=1, comp_adj=True, d=None):
-        proportion_avg = Simulator.simulate_rw_on_rg_visited_prop(n, p, k, comp_adj, d)
+        proportion_avg = Simulator.simulate_rw_on_rg_visited_prop(
+            n, p, k, comp_adj, d)
         steps = len(proportion_avg)-1
 
         c = np.arange(0, 1, 0.1)*steps
@@ -83,50 +99,3 @@ class Simulator:
         plt.ylabel("Proportion of visited vertices")
         plt.legend()
         plt.show()
-
-
-    @staticmethod
-    def simulate_walk_greedy(graph, walker, steps, calculate_prop = True):
-        walker.reset()
-        walker.load_graph(graph)
-        n = len(graph)
-
-        walk = [walker.cur]
-        proportions = np.ones(steps+1)
-
-        for i in range(1,steps +1):
-            new_node = walker.move()
-            walk.append(walker.cur)
-            if new_node:
-                proportions[i] = proportions[i-1] + 1
-            else:
-                proportions[i] = proportions[i-1]
-
-        results = {
-            'walk': walk,
-            }
-        if calculate_prop:
-            results['proportions'] = proportions/n
-
-        return results
-
-
-    @staticmethod
-    def simulate_gu_on_rg_visited_prop(n, d, steps, k=1, comp_adj=True):
-        greedy_walker = GreedyWalker()
-        proportion_sum = np.zeros(steps+1)
-
-        threshold = largest_comp(d)*0.8 
-        valid_samples = 0
-        while valid_samples < k:
-            graph = Graph.get_er_random_graph(n, d/n)
-            results = Simulator.simulate_walk_greedy(graph, greedy_walker, steps)
-            if results['proportions'][-1] >= threshold:
-                proportion_sum += results['proportions']
-                valid_samples += 1
-        proportion_avg = proportion_sum / k
-        return proportion_avg
-
-
-data = Simulator.simulate_gu_on_rg_visited_prop(1000,10,10000, 50)
-print(data)
