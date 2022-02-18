096e06b798f38c77059e26cc5677994097baa4f6
Andrew Yin <andrewyin1@gmail.com>
2022-02-18 09:51:03 -0600

    Add numerical solving to largest comp function

--- utils.py
+++ utils.py
@@ -1,16 +1,13 @@
+from sympy.solvers import solve
+from sympy import Symbol
+from sympy import log
+
+
 def largest_comp(d):
     """
     Returns the expected size (as a proportion of nodes) of the largest component in a random graph given expected degree d
     # TODO: Possibly numerically solve this
     """
-    if d == 2:
-        f_d = 0.797
-    elif d == 3:
-        f_d = 0.94
-    elif d == 4:
-        f_d = 0.98
-    elif d == 5:
-        f_d = 0.993
-    elif d == 10:
-        f_d = 0.9999
-    return f_d
+    x = Symbol('x')
+    equation = d*x + log(1-x)
+    return round(float(solve(d*x + log(1-x), x)[1]), 5)
