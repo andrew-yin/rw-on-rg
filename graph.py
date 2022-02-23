081efd270289a3106fbf0759dd04bd6a874fdc58
Andrew Yin <andrewyin1@gmail.com>
2022-02-22 18:15:56 -0600

    Add k regular graph simulations  (#6)
    * k_regular
    
    * Add files via upload
    
    * Add check for component exclusion (#3)
    
    * Add photo example
    
    * Fix project structure
    
    * Create README.md
    
    * Add plot of visited prop. vs c (#4)
    
    * Add greedy unbiased walker (#5)
    
    * add the greedy walker and fix some minor issue with the random walker
    
    Add a new greedy walker and its simulation function. delete p for random graph since p = d/n, add the threshold.
    
    * Refactor
    
    Co-authored-by: anecnhoj53 <98916161+anecnhoj53@users.noreply.github.com>
    
    * Add greedy unbiased walker
    
    * Change threshold to half
    
    * Add visited prop vs c. simulation results
    
    * Update README.md
    
    * Add numerical solving to largest comp function
    
    * Add Xiangrui's log transformation notebook
    
    * Add files via upload
    
    * k_regular
    
    * expected_d
    
    * Refactor project structure
    
    Co-authored-by: Yang <troyyang2001@gmail.com>
    Co-authored-by: tcyang54 <89712018+tcyang54@users.noreply.github.com>
    Co-authored-by: anecnhoj53 <98916161+anecnhoj53@users.noreply.github.com>

--- graph.py
+++ graph.py
@@ -1,7 +1,25 @@
 import networkx as nx
+import math
 
 class Graph:
 
     @staticmethod
     def get_er_random_graph(n : int, p : float):
         return nx.gnp_random_graph(n, p)
+
+    @staticmethod
+    def get_k_regular_random_graph(d: int, n: int):
+        return nx.random_regular_graph(d, n)
+
+    # This method generates graphs with an expected degree of d
+    @staticmethod
+    def get_expected_d_random_graph(d: [], p: [], n: int):
+        # n- total number of nodes, d: a list of degrees
+        z1, z2 = [], []
+        for i in range(math.ceil(p[0]*n)):
+            z1.append(d[0])
+        for i in range(math.floor(p[1]*n)):
+            z2.append(d[1])
+        z = z1+z2
+        # expected_d = d[0]*p[0] + d[1]*p[1]
+        return nx.expected_degree_graph(z)
